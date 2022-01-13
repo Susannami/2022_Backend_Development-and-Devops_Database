756e6bf81d815d15ef7813cb1785a21b6810ad63
Sami Kojo-Fry <sami.kojo@tuni.fi>
2022-01-13 18:00:04 +0200

    Exercise 1

---
+++ Exercise 1/task4.sql
@@ -0,0 +1,3 @@
+-- Retrieve name and length for tracks which length is less than one minute.
+-- Sort the result primarily in ascending order by the name and secondarily 
+-- in descending order by the length.
