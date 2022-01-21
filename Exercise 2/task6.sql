2cefb79a13901db50dffa1c74dbe86e00e86996c
Sami Kojo-Fry <69346747+samikojo-tuni@users.noreply.github.com>
2022-01-21 12:06:04 +0200

    Fix: Typo

--- Exercise 2/task6.sql
+++ Exercise 2/task6.sql
@@ -1,3 +1,3 @@
-l-- Calculate and return the count of albums each band has in the database.
+-- Calculate and return the count of albums each band has in the database.
 -- Include Artist name and album count into the result.
 -- Name the result columns as "Artist" and "Album count".
