ad80c51b667acd155ee71064f5e320e2e71f9981
Sami Kojo-Fry <sami.kojo@tuni.fi>
2022-01-28 12:17:28 +0200

    Lesson 3 material

--- Material/Lesson 2/class.sql
+++ Material/Lesson 2/class.sql
@@ -97,3 +97,10 @@ FROM subjects;
 
 SELECT SUM(subjects.credits) AS "Total credits"
 FROM subjects;
+
+SELECT subjects.subject_name as "Subject", COUNT(student_id) AS "Student count", YEAR(courses.start_date) AS year
+FROM enrollments
+INNER JOIN courses ON enrollments.course_id = courses.id
+INNER JOIN subjects ON courses.subject_id = subjects.id
+GROUP BY courses.id
+HAVING year = 2022;
