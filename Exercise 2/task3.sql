3e608bd46617ca0ed37a5f7d9dc4867955389ab2
Sami Kojo-Fry <sami.kojo@tuni.fi>
2022-02-04 12:23:52 +0200

    Exercise 2 solutions

--- Exercise 2/task3.sql
+++ Exercise 2/task3.sql
@@ -1,2 +1,4 @@
 -- Calculate and return the count of albums in the database.
--- Name the count as album_count.
+-- Name the count column as album_count.
+SELECT COUNT(*) as album_count
+FROM album;
