99574d0edd23ebe2ef38207e9f471f8b7a3123f6
Sami Kojo-Fry <sami.kojo@tuni.fi>
2022-02-17 12:12:11 +0200

    Exercise 5 solutions

--- Exercise 5/task1.sql
+++ Exercise 5/task1.sql
@@ -1,3 +1,48 @@
 -- Create a database based on last week's task 3. In that task we designed a database for a blog. The database has
 -- to follow the plan created last week. If you need to update the plan, include an updated drawing of the
 -- database to your return. Add foreign key restrictions for the tables as well.
+
+CREATE TABLE `post`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `headline` VARCHAR(127) NOT NULL,
+    `content` TEXT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` INT NOT NULL,
+    PRIMARY KEY(`id`),
+		FOREIGN KEY(`created_by`) REFERENCES `user`(`id`)
+			ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE `user`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(63),
+    `last_name` VARCHAR(63),
+    `password` CHAR(192) NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `role_id` INT NOT NULL,
+    PRIMARY KEY(`id`),
+		FOREIGN KEY(`role_id`) REFERENCES `role`(`id`)
+		ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE `comment`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `headline` VARCHAR(127) NOT NULL,
+    `content` TEXT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` INT NOT NULL,
+    `post_id` INT NOT NULL,
+    PRIMARY KEY(`id`),
+		FOREIGN KEY(`created_by`) REFERENCES `user`(`id`)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+		FOREIGN KEY(`post_id`) REFERENCES `post`(`id`)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+);
+
+CREATE TABLE `role`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(31) NOT NULL,
+    PRIMARY KEY(`id`)
+);
--- Exercise 5/task2.sql
+++ Exercise 5/task2.sql
@@ -1,3 +1,18 @@
 -- Add users and roles for the Blog-database. Create roles "author_role" and "commenter_role" and grant those
 -- roles appropriate privileges. Add users "author_app" and "commenter_app". The imaginary webapp uses them for
 -- adding posts and comments to the blog database. Grant appropriate roles for those users.
+
+CREATE USER author_app IDENTIFIED BY "AuthorPassword";
+CREATE USER commenter_app IDENTIFIED BY "CommenterPassword";
+
+CREATE ROLE author_role;
+CREATE ROLE commenter_role;
+
+GRANT INSERT, SELECT, UPDATE ON post TO author_role;
+GRANT INSERT, SELECT, UPDATE ON comment TO commenter_role;
+
+GRANT author_role, commenter_role TO author_app;
+GRANT commenter_role TO commenter_app;
+
+SET DEFAULT ROLE author_role, commenter_role TO author_app;
+SET DEFAULT ROLE commenter_role TO commenter_app;
